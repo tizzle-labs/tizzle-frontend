61cfd8fb2427386c774f3d8e036e5b3e77ebd324
iqbalutomo <muhlisiqbalutomo@gmail.com>
2024-10-06 15:48:49 +0700

    feat: bale agent

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 'use client';
 
-import Akira from '@tizzle-fe/components/agent/akira/Akira';
 import useStore from '@tizzle-fe/stores/userStore';
 import Cortez from '@tizzle-fe/components/agent/cortez/Cortez';
+import Akira from '@tizzle-fe/components/agent/akira/Akira';
+import Bale from '@tizzle-fe/components/agent/bale/Bale';
 
 export default function Home() {
   const selectedAgent = useStore(state => state.selectedAgent);
@@ -12,8 +13,9 @@ export default function Home() {
       case 'cortez':
         return <Cortez />;
       case 'akira':
-      default:
         return <Akira />;
+      default:
+        return <Bale />;
     }
   };
 
