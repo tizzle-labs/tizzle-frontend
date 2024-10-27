e29765930b9af936421f79e4d3f42611fc7a2883
Ahnaf <ahnaf@mail.com>
2024-10-27 16:01:25 +0700

    fix: suspense search params

--- src/app/agent/bale/page.js
+++ src/app/agent/bale/page.js
@@ -2,15 +2,21 @@
 
 import { AgentCanvas } from '@tizzle-fe/components/interactions/AgentCanvas';
 import { usePathname } from 'next/navigation';
-import { Suspense } from 'react';
+import { Suspense, useSearchParams } from 'react';
 
 function BalePage() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
   const agentPath = pathname.split('/').filter(Boolean).pop();
+  const transactionHashes = searchParams.get('transactionHashes');
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <AgentCanvas agentPath={agentPath} />;
+      <AgentCanvas
+        agentPath={agentPath}
+        transactionHashes={transactionHashes}
+      />
+      ;
     </Suspense>
   );
 }
--- src/app/agent/cortez/page.js
+++ src/app/agent/cortez/page.js
@@ -2,15 +2,20 @@
 
 import { Suspense } from 'react';
 import { AgentCanvas } from '@tizzle-fe/components/interactions/AgentCanvas';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 function CortezPage() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
   const agentPath = pathname.split('/').filter(Boolean).pop();
+  const transactionHashes = searchParams.get('transactionHashes');
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <AgentCanvas agentPath={agentPath} />;
+      <AgentCanvas
+        agentPath={agentPath}
+        transactionHashes={transactionHashes}
+      />
     </Suspense>
   );
 }
