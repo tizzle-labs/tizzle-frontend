9cb664610b8f301ddf0929af2b29d13c808ee560
iqbalutomo <muhlisiqbalutomo@gmail.com>
2024-10-28 21:50:57 +0700

    fix: adjustment code

--- src/components/interactions/AgentInteraction.jsx
+++ src/components/interactions/AgentInteraction.jsx
@@ -317,7 +317,7 @@ export const AgentInteraction = ({ agentPath, hidden, transactionHashes }) => {
       />
       <div className="w-full flex flex-col items-end justify-center gap-4"></div>
       <div className="flex items-center gap-2 pointer-events-auto max-w-screen-sm w-full mx-auto">
-        <button
+        {/* <button
           disabled={loading}
           onClick={() => alert('recording brooo...')}
           className={`bg-black hover:bg-red-500 text-white p-3 px-3 font-semibold uppercase rounded-full ${loading ? 'cursor-not-allowed opacity-30' : ''}`}
@@ -336,7 +336,7 @@ export const AgentInteraction = ({ agentPath, hidden, transactionHashes }) => {
               d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
             />
           </svg>
-        </button>
+        </button> */}
 
         <input
           ref={inputRef}
